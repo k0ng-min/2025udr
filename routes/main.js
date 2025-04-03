c9bc2fad84e2756b3fd1c174eddbcb940f4e2445
설경민 <skmin3735@gmail.com>
2025-04-03 18:28:36 +0900

    first page finised - post page not connected css

--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const mainLayout = "../views/layouts/main.ejs";
+const Post = require("../models/Post");
+const asyncHandler = require("express-async-handler");
 
-router.get(["/", "/home"], (req,res)=>{
+router.get(["/", "/home"], asyncHandler(async(req,res)=>{
     const locals= {
         title: "Home"
     };
-    res.render("index", { locals, layout: mainLayout });
-});
+    const data = await Post.find();
+    res.render("index", { locals, data, layout: mainLayout });
+}));
 
 router.get("/about", (req,res)=>{
     const locals= {
@@ -16,4 +19,22 @@ router.get("/about", (req,res)=>{
     res.render("about", { locals, layout: mainLayout });
 });
 
+router.get("/post/:id", asyncHandler(async(req,res)=>{
+    const data = await Post.findOne({_id: req.params.id});
+    res.render("post", { data, layout: mainLayout });
+}));
+
 module.exports = router;
+
+// Post.insertMany([
+//     { title: "1st Post", body: "This is the body of the 1st post" },
+//     { title: "2st Post", body: "This is the body of the 2st post" },
+//     { title: "3st Post", body: "This is the body of the 3st post" },
+//     { title: "4st Post", body: "This is the body of the 4st post" },
+//     { title: "5st Post", body: "This is the body of the 5st post" },
+//     { title: "6st Post", body: "This is the body of the 6st post" },
+//     { title: "7st Post", body: "This is the body of the 7st post" },
+//     { title: "8st Post", body: "This is the body of the 8st post" },
+//     { title: "9st Post", body: "This is the body of the 9st post" },
+//     { title: "10st Post", body: "This is the body of the 10st post" }
+// ]);
